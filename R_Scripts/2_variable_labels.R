137d8aca3cc22fef35fb66a9e91d6f0050c722bc
Simon Kiss <sjkiss@gmail.com>
2022-06-07 15:44:20 -0400

    Upload

--- R_Scripts/2_variable_labels.R
+++ R_Scripts/2_variable_labels.R
@@ -8,3 +8,7 @@ on22 %>%
 #Please look in the data dictionary and provide a meaningful, systematic variable label for each one
 
 var_label(on22$Q31_1)<-c("Affordability of $800 per month")
+
+# 
+var_label(on22$Q32_1)<-c("Causes - Speculation")
+var_label(on22$Q32_2)<-c("Causes - Low Interest Rates")
