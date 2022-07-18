936c4a8738182d123ee01171d3784e51c4e33aca
Simon Kiss <sjkiss@gmail.com>
2022-07-18 16:12:48 -0400

    Descriptives Changes

--- R_Scripts/3_descriptives.R
+++ R_Scripts/3_descriptives.R
@@ -1,5 +1,15 @@
 #### This script is for descriptive statistics and tables
 #Table of Descriptives for Housing Status
-library(tabyl)
-kbl(tabyl(on22$Housing_Status, sort= TRUE)) %>% 
-  kable_styling()
+library(janitor)
+#Housng Status
+tabyl(on22$Housing_Status, sort= TRUE) 
+names(on22)
+on22 %>% 
+  as_factor() %>% 
+  tabyl(Housing_Status, Q6b)
+
+#Cross tab 2018 vote by 2021 vote 
+
+#Cross tab vote switching variable by housing status
+
+# Other stuff! Feel free to freelance. 
