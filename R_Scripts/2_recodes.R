8134117d54d76d4860e872f4d2d2530c738f3c9a
Simon Kiss <sjkiss@gmail.com>
2025-05-02 11:20:23 -0400

    Uploading Tim's Scripts

--- R_Scripts/2_recodes.R
+++ R_Scripts/2_recodes.R
@@ -15,7 +15,7 @@ library(car)
 # All others
 
 #Recoding Q6b to simplify party names
-on22$Vote<-Recode(as.numeric(on22$Q6b), "1='Liberal'; 2='PC' ; 3='NDP' ; 4='Green'")
+data.5$Vote<-car::Recode(as.numeric(data.5$Q6b), "1='Liberal'; 2='PC' ; 3='NDP' ; 4='Green'")
 on22 %>% 
   mutate(Vote=case_when(
     Q6b==1~"PC",
--- R_Scripts/2_statscan_census_data.R
+++ R_Scripts/2_statscan_census_data.R
@@ -39,43 +39,48 @@ on_statscan_da %>%
 names(on_statscan_da)
 on_statscan_da %>% 
   rename_with(., ~paste0(., "_da"))->on_statscan_da
-
+#This drops superfluous variables that we don't need for merging with ON22
+# Tim has already got these
+names(on_statscan_da)
+on_statscan_da %>% 
+  select(DA2021=1, 12:28)->on_statscan_da
+#This writes the data out for the record.
 write_csv(on_statscan_da, file=here("Data/ontario_statscan_data_da.csv"))
 #Get CSD level data
 #Get CSD data
-on_statscan_csd <- get_census(dataset='CA21',regions=list(PR="35"),
-                          vectors=c("v_CA21_434","v_CA21_435","v_CA21_440","v_CA21_436","v_CA21_437","v_CA21_438","v_CA21_439","v_CA21_4290", "v_CA21_4309", "v_CA21_4317"),
-                          labels="detailed", geo_format=NA, level='CSD')
-#Get CSD data
-names(on_statscan_csd)
-on_statscan_csd %>%
-  rename("total_occupied_private_dwellings"=11,
-         "single_detached_houses"=12,
-         "apartment_in_building_plus_5"=13,
-         "semi_detached_house"=14,
-         "row_house"=15,
-         "apartment_in_duplex"=16,
-         "apartment_in_building_less_5"=17,
-         "households_more_than_30"=18,
-         "median_shelter_costs_owned"=19,
-         "median_shelter_costs_rented"=20
-  )->on_statscan_csd
-
-on_statscan_csd %>%
-  mutate(single_detached_houses_pct=single_detached_houses/total_occupied_private_dwellings,
-         semi_detached_house_pct=semi_detached_house/total_occupied_private_dwellings,
-         row_house_pct=row_house/total_occupied_private_dwellings,
-         apartment_in_duplex_pct=apartment_in_duplex/total_occupied_private_dwellings,
-         apartment_in_building_less_5_pct=apartment_in_building_less_5/total_occupied_private_dwellings,
-         apartment_in_building_plus_5_pct=apartment_in_building_plus_5/total_occupied_private_dwellings)->on_statscan_csd
-#Create population density
-on_statscan_csd %>%
-  mutate(pop_density=Population/`Area (sq km)`)->on_statscan_csd
-
-on_statscan_csd %>% 
-  rename_with(., ~paste0(., "_csd"))->on_statscan_csd
-write_csv(on_statscan_csd, file=here("Data/ontario_statscan_data_csd.csv"))
-
+# on_statscan_csd <- get_census(dataset='CA21',regions=list(PR="35"),
+#                           vectors=c("v_CA21_434","v_CA21_435","v_CA21_440","v_CA21_436","v_CA21_437","v_CA21_438","v_CA21_439","v_CA21_4290", "v_CA21_4309", "v_CA21_4317"),
+#                           labels="detailed", geo_format=NA, level='CSD')
+# #Get CSD data
+# names(on_statscan_csd)
+# on_statscan_csd %>%
+#   rename("total_occupied_private_dwellings"=11,
+#          "single_detached_houses"=12,
+#          "apartment_in_building_plus_5"=13,
+#          "semi_detached_house"=14,
+#          "row_house"=15,
+#          "apartment_in_duplex"=16,
+#          "apartment_in_building_less_5"=17,
+#          "households_more_than_30"=18,
+#          "median_shelter_costs_owned"=19,
+#          "median_shelter_costs_rented"=20
+#   )->on_statscan_csd
+# 
+# on_statscan_csd %>%
+#   mutate(single_detached_houses_pct=single_detached_houses/total_occupied_private_dwellings,
+#          semi_detached_house_pct=semi_detached_house/total_occupied_private_dwellings,
+#          row_house_pct=row_house/total_occupied_private_dwellings,
+#          apartment_in_duplex_pct=apartment_in_duplex/total_occupied_private_dwellings,
+#          apartment_in_building_less_5_pct=apartment_in_building_less_5/total_occupied_private_dwellings,
+#          apartment_in_building_plus_5_pct=apartment_in_building_plus_5/total_occupied_private_dwellings)->on_statscan_csd
+# #Create population density
+# on_statscan_csd %>%
+#   mutate(pop_density=Population/`Area (sq km)`)->on_statscan_csd
+# 
+# on_statscan_csd %>% 
+#   rename_with(., ~paste0(., "_csd"))->on_statscan_csd
+# write_csv(on_statscan_csd, file=here("Data/ontario_statscan_data_csd.csv"))
+# 
 #Read in Shelter costs
 #Import the data file on shelter costs in the Canadian census
 # on_statscan_da<-read.csv(file=here("Data/ontario_statscan_data_da.csv"))
