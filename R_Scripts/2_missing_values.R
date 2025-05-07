fee5a8cc246e9007a14994ea9fa105cc690a4139
Simon Kiss <sjkiss@gmail.com>
2025-05-07 15:34:51 -0400

    Saved missing values

--- R_Scripts/2_missing_values.R
+++ R_Scripts/2_missing_values.R
@@ -12,10 +12,10 @@ on22%>%
 on22%>%
   mutate(da_good = ifelse(nchar(DA2021)==8, "1", NA))->on22
 
-on22 %>% 
-  as_factor() %>% 
-  select(da_good, postal_code_good, Housing_Status, Experimental_Group, Q23) %>% 
-  summarise(across(everything(), ~sum(is.na(.))))
+# on22 %>% 
+#   as_factor() %>% 
+#   select(da_good, postal_code_good, Housing_Status, Experimental_Group, Q23) %>% 
+#   summarise(across(everything(), ~sum(is.na(.))))
 names(on22)
 on22%>%
 mutate(across(c(Q1,Q2,Q4_1,Q5_1,Q6a,Q6b,Q7,Q8,Q9,Q10,Q11,Q12_1:Q12_7,,Q13_1:Q13_4,Q14_1:Q14_4,Q15_1:Q15_7,Q16:Q21,Q22a_1:Q22a_3,Q24:Q26,Q27, Q31_1:Q31_6,Q32_1:Q32_9,Q33a_1:Q33a_6,Q80_1:Q80_4,Q34_1:Q35_6, Q36,Q38,Q39,Q40,Q41,Q44),~ case_when(is.na(.x) ~ 1, TRUE ~ 0), .names = "{.col}_miss")) %>% 
@@ -28,7 +28,7 @@ on22 %>%
 on22 %>% 
   select(sum_missing, sum_seen, missing_pct) 
   on22 %>% 
-    select(sum_missing, sum_seen, missing_pct) %>% 
+    select(sum_missing, sum_seen, missing_pct, duration_minutes) %>% 
   filter(.,missing_pct>100)
   on22 %>% 
     filter(., missing_pct<100) %>% 
